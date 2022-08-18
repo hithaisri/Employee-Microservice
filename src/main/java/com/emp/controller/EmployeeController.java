b3d67c60afbca5e79ad62cd7691f84b31fb9837a
hithaisri <hithaisri.97@gmail.com>
2022-08-18 12:54:18 +0530

    Rest template added for microservice interaction

--- src/main/java/com/emp/controller/EmployeeController.java
+++ src/main/java/com/emp/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.emp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.emp.entity.Employee;
 import com.emp.service.IEmployeeService;
@@ -16,9 +19,16 @@ public class EmployeeController {
 	@Autowired
 	IEmployeeService employeeService;
 	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@GetMapping("/{eid}")
 	public Employee getEmployee(@PathVariable("eid") Long id) {
-		return employeeService.getEmployee(id);
-		
+		Employee employee= employeeService.getEmployee(id);
+		List records = this.restTemplate.getForObject("http://localhost:9090/record/emp/"+id, List.class);
+		employee.setRecords(records);
+		return employee;
 	}
+	
+	
 }
